456a0d1fa6703b5167fa76d909674d93a9944a1f
ugurbagkuran <ugurbagkuranu@gmail.com>
2025-12-03 00:48:54 +0300

    yeni router eklendi

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from app.core.config import settings
 from app.core.database import connect_to_mongo, close_mongo_connection
 from app.services.movies.routes import router as movies_router
+from app.services.auth.routes import router as auth_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,6 +18,7 @@ app = FastAPI(
 )
 
 app.include_router(movies_router, prefix="/movies", tags=["Movies"])
+app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 
 # if __name__ == "__main__":
 #     import uvicorn
